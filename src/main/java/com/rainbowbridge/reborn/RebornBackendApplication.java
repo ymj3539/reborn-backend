9f8b9c7e456eb37c1565df4d8d31dd4a8be90689
Gomson-E <gomsonixx@gmail.com>
2023-11-13 19:30:53 +0900

    Initial Commit

---
+++ src/main/java/com/rainbowbridge/reborn/RebornBackendApplication.java
@@ -0,0 +1,13 @@
+package com.rainbowbridge.reborn;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RebornBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(RebornBackendApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/rainbowbridge/reborn/RebornBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.rainbowbridge.reborn;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RebornBackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
