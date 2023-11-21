a3ef5c9ef5ceb58a5b1c06ef8603d73977a84900
Gomson-E <gomsonixx@gmail.com>
2023-11-21 22:47:00 +0900

    UPDATE : ApiOperation 추가

--- src/main/java/com/rainbowbridge/reborn/controller/UserController.java
+++ src/main/java/com/rainbowbridge/reborn/controller/UserController.java
@@ -4,6 +4,7 @@ import com.rainbowbridge.reborn.domain.User;
 import com.rainbowbridge.reborn.dto.user.LoginDto;
 import com.rainbowbridge.reborn.dto.user.UserAddDto;
 import com.rainbowbridge.reborn.service.UserService;
+import io.swagger.annotations.ApiOperation;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -22,37 +23,29 @@ public class UserController {
 
     private final UserService userService;
 
-    /**
-     *  아이디 중복 확인
-     */
     @GetMapping("/{id}")
+    @ApiOperation(value="아이디 중복 확인")
     public ResponseEntity checkDuplicatedId(@PathVariable String id) {
         userService.checkDuplicatedId(id);
         return ResponseEntity.ok("사용 가능한 아이디입니다.");
     }
 
-    /**
-     *  회원가입
-     */
     @PostMapping
+    @ApiOperation(value="회원가입")
     public ResponseEntity addUser(@RequestBody UserAddDto dto, HttpSession session){
         userService.addUser(dto, session);
         return ResponseEntity.ok("로그인에 성공했습니다");
     }
 
-    /**
-     *  로그인
-     */
     @PostMapping("/login")
+    @ApiOperation(value="로그인")
     public ResponseEntity loginUser(@RequestBody LoginDto dto, HttpSession session) {
         userService.loginUser(dto, session);
         return ResponseEntity.ok("로그인에 성공했습니다.");
     }
 
-    /**
-     *  로그아웃
-     */
     @GetMapping("/logout")
+    @ApiOperation(value="로그아웃")
     public ResponseEntity logout(HttpSession session) {
         session.removeAttribute("user");
         return ResponseEntity.ok("로그아웃에 성공했습니다.");
