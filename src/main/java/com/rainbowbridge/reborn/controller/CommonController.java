0b9f03f7f846e7bfcc5ae1891c90adfac136c304
Gomson-E <gomsonixx@gmail.com>
2023-11-24 22:14:40 +0900

    REFACTOR : Controller 구조 변경

---
+++ src/main/java/com/rainbowbridge/reborn/controller/CommonController.java
@@ -0,0 +1,38 @@
+package com.rainbowbridge.reborn.controller;
+
+import com.rainbowbridge.reborn.dto.common.DayResponseDto;
+import io.swagger.annotations.ApiOperation;
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.time.LocalDate;
+import java.time.format.TextStyle;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Locale;
+
+@RestController
+@RequiredArgsConstructor
+@RequestMapping("/api/common")
+public class CommonController {
+
+    @GetMapping("/calendar")
+    @ApiOperation(value = "캘린더 일자 조회")
+    public List<DayResponseDto> getDayList() {
+        List<DayResponseDto> dtos = new ArrayList<>();
+        LocalDate today = LocalDate.now();
+
+        for (int i = 0; i < 7; i++) {
+            LocalDate date = today.plusDays(i);
+            int day = date.getDayOfMonth();
+            String dayOfWeek = date.getDayOfWeek().getDisplayName(TextStyle.NARROW, Locale.KOREAN);
+
+            dtos.add(new DayResponseDto(day, dayOfWeek));
+        }
+
+        return dtos;
+    }
+
+}
--- src/main/java/com/rainbowbridge/reborn/controller/CompanyController.java
+++ src/main/java/com/rainbowbridge/reborn/controller/CompanyController.java
@@ -1,7 +1,5 @@
 package com.rainbowbridge.reborn.controller;
 
-import com.rainbowbridge.reborn.domain.Company;
-import com.rainbowbridge.reborn.dto.common.DayResponseDto;
 import com.rainbowbridge.reborn.dto.company.CalendarCompanyListRequestDto;
 import com.rainbowbridge.reborn.dto.company.CalendarCompanyListResponseDto;
 import com.rainbowbridge.reborn.service.CompanyService;
@@ -12,41 +10,19 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-import java.time.LocalDate;
-import java.time.format.TextStyle;
-import java.util.ArrayList;
 import java.util.List;
-import java.util.Locale;
 
 @RestController
 @RequiredArgsConstructor
-@RequestMapping("/api/calendar")
-public class CalendarController {
+@RequestMapping("/api/companies")
+public class CompanyController {
 
     private final CompanyService companyService;
 
-    @GetMapping
-    @ApiOperation(value = "캘린더 일자 조회")
-    public List<DayResponseDto> getDayList() {
-        List<DayResponseDto> dtos = new ArrayList<>();
-        LocalDate today = LocalDate.now();
-
-        for (int i = 0; i < 7; i++) {
-            LocalDate date = today.plusDays(i);
-            int day = date.getDayOfMonth();
-            String dayOfWeek = date.getDayOfWeek().getDisplayName(TextStyle.NARROW, Locale.KOREAN);
-
-            dtos.add(new DayResponseDto(day, dayOfWeek));
-        }
-
-        return dtos;
-    }
-
-    @GetMapping("/companies")
+    @GetMapping("/calendar")
     @ApiOperation(value = "바로예약 캘린더 업체 리스트 조회")
     public List<CalendarCompanyListResponseDto> getCalendarCompanyList(@RequestBody CalendarCompanyListRequestDto dto) {
         return companyService.getCalendarCompanyList(dto);
     }
-
 
 }
