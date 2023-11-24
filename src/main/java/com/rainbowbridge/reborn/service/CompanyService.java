e680da486b7b39d20cdc5926d4479e23974811af
Gomson-E <gomsonixx@gmail.com>
2023-11-24 22:54:31 +0900

    REFACTOR : 거리 계산 함수 CommonService로 분리

--- src/main/java/com/rainbowbridge/reborn/service/CompanyService.java
+++ src/main/java/com/rainbowbridge/reborn/service/CompanyService.java
@@ -22,6 +22,7 @@ import java.util.stream.Collectors;
 public class CompanyService {
 
     private final CompanyRepository companyRepository;
+    private final CommonService commonService;
 
     public List<CompanyListDto> getCalendarCompanyList(CalendarCompanyListRequestDto dto) {
         // 영업 가능한 업체 조회
@@ -60,7 +61,7 @@ public class CompanyService {
         // 각 업체의 사용자의 거리를 계산하고, 그 결과를 Map에 저장
         Map<Company, Double> companyDistanceMap = new HashMap<>();
         for (Company company : companies) {
-            double distance = calculateDistance(userLatitude, userLongitude, company.getLatitude(), company.getLongitude());
+            double distance = commonService.calculateDistance(userLatitude, userLongitude, company.getLatitude(), company.getLongitude());
             companyDistanceMap.put(company, distance);
         }
 
@@ -77,41 +78,4 @@ public class CompanyService {
         return nearestCompanies;
     }
 
-    // Vincenty 공식 사용
-    private double calculateDistance(double userLatitude, double userLongitude, double companyLatitude, double companyLongitude) {
-        double a = 6378137, b = 6356752.314245, f = 1 / 298.257223563;  // WGS-84 ellipsoid params
-        double L = Math.toRadians(companyLongitude - userLongitude);
-        double U1 = Math.atan((1 - f) * Math.tan(Math.toRadians(userLatitude)));
-        double U2 = Math.atan((1 - f) * Math.tan(Math.toRadians(companyLatitude)));
-        double sinU1 = Math.sin(U1), cosU1 = Math.cos(U1);
-        double sinU2 = Math.sin(U2), cosU2 = Math.cos(U2);
-
-        double sinLambda, cosLambda, sinSigma, cosSigma, sigma, sinAlpha, cosSqAlpha, cos2SigmaM;
-        double lambda = L, lambdaP, iterLimit = 100;
-        do {
-            sinLambda = Math.sin(lambda);
-            cosLambda = Math.cos(lambda);
-            sinSigma = Math.sqrt((cosU2 * sinLambda) * (cosU2 * sinLambda) + (cosU1 * sinU2 - sinU1 * cosU2 * cosLambda) * (cosU1 * sinU2 - sinU1 * cosU2 * cosLambda));
-            if (sinSigma == 0) return 0;  // co-incident points
-            cosSigma = sinU1 * sinU2 + cosU1 * cosU2 * cosLambda;
-            sigma = Math.atan2(sinSigma, cosSigma);
-            sinAlpha = cosU1 * cosU2 * sinLambda / sinSigma;
-            cosSqAlpha = 1 - sinAlpha * sinAlpha;
-            cos2SigmaM = cosSigma - 2 * sinU1 * sinU2 / cosSqAlpha;
-            if (Double.isNaN(cos2SigmaM)) cos2SigmaM = 0;  // equatorial line: cosSqAlpha=0 (§6)
-            double C = f / 16 * cosSqAlpha * (4 + f * (4 - 3 * cosSqAlpha));
-            lambdaP = lambda;
-            lambda = L + (1 - C) * f * sinAlpha * (sigma + C * sinSigma * (cos2SigmaM + C * cosSigma * (-1 + 2 * cos2SigmaM * cos2SigmaM)));
-        } while (Math.abs(lambda - lambdaP) > 1e-12 && --iterLimit > 0);
-
-        if (iterLimit == 0) return 0;  // formula failed to converge
-
-        double uSq = cosSqAlpha * (a * a - b * b) / (b * b);
-        double A = 1 + uSq / 16384 * (4096 + uSq * (-768 + uSq * (320 - 175 * uSq)));
-        double B = uSq / 1024 * (256 + uSq * (-128 + uSq * (74 - 47 * uSq)));
-        double deltaSigma = B * sinSigma * (cos2SigmaM + B / 4 * (cosSigma * (-1 + 2 * cos2SigmaM * cos2SigmaM) - B / 6 * cos2SigmaM * (-3 + 4 * sinSigma * sinSigma) * (-3 + 4 * cos2SigmaM * cos2SigmaM)));
-        double s = b * A * (sigma - deltaSigma);
-
-        return s;
-    }
 }
