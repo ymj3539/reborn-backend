fde3435e0a5b19c26e7d40fd71ce14a364323784
Gomson-E <gomsonixx@gmail.com>
2023-12-04 01:15:04 +0900

    FEAT : 찜 목록 조회 Controller

--- src/main/java/com/rainbowbridge/reborn/controller/HeartController.java
+++ src/main/java/com/rainbowbridge/reborn/controller/HeartController.java
@@ -1,15 +1,18 @@
 package com.rainbowbridge.reborn.controller;
 
+import com.rainbowbridge.reborn.dto.heart.HeartListDto;
 import com.rainbowbridge.reborn.service.HeatService;
 import io.swagger.annotations.ApiOperation;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import javax.servlet.http.HttpSession;
+import java.util.List;
 
 @RestController
 @RequiredArgsConstructor
@@ -23,6 +26,12 @@ public class HeartController {
     public ResponseEntity add(@PathVariable String companyId, HttpSession session) {
        heatService.addHeart(companyId, session);
        return ResponseEntity.ok("찜 하기에 성공했습니다");
+    }
+
+    @GetMapping
+    @ApiOperation(value = "찜한 업체 목록 조회")
+    public List<HeartListDto> add(HttpSession session) {
+        return heatService.getHeartList(session);
     }
 
 }
