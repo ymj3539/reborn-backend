2ae7a64a01347f313d57627af19296696e47494e
Gomson-E <gomsonixx@gmail.com>
2023-12-04 01:14:50 +0900

    FEAT : 찜 목록 조회 응답 DTO

---
+++ src/main/java/com/rainbowbridge/reborn/dto/heart/HeartListDto.java
@@ -0,0 +1,20 @@
+package com.rainbowbridge.reborn.dto.heart;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+public class HeartListDto {
+
+    private String id;
+
+    private String name;
+
+    private String address;
+
+}
