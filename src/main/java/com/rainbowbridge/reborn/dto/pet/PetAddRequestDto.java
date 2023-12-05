67b65c7486e3ba4a05520c2bbb33d9ac35692385
Gomson-E <gomsonixx@gmail.com>
2023-12-05 23:33:37 +0900

    FEAT : 반려동물 정보 추가 요청 DTO

---
+++ src/main/java/com/rainbowbridge/reborn/dto/pet/PetAddRequestDto.java
@@ -0,0 +1,46 @@
+package com.rainbowbridge.reborn.dto.pet;
+
+import com.rainbowbridge.reborn.domain.Species;
+import io.swagger.annotations.ApiModelProperty;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.NotNull;
+
+@Data
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+public class PetAddRequestDto {
+
+    @NotNull
+    @ApiModelProperty(value = "구분", example = "DOG")
+    private Species species;
+
+    @NotBlank
+    @ApiModelProperty(value = "종", example = "말티즈")
+    private String breed;
+
+    @NotBlank
+    @ApiModelProperty(value = "이름", example = "복실이")
+    private String name;
+
+    @NotNull
+    @ApiModelProperty(value = "나이 - 년", example = "9")
+    private int years;
+
+    @NotNull
+    @ApiModelProperty(value = "나이 - 개월", example = "10")
+    private int months;
+
+    @NotNull
+    @ApiModelProperty(value = "체중", example = "5.7")
+    private double weight;
+
+
+
+
+}
