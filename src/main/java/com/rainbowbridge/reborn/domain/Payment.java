de243e714a8f797e38a994444bd9c3b70050b48d
Gomson-E <gomsonixx@gmail.com>
2023-12-05 23:33:43 +0900

    FEAT : Payment Enum 값 설정

--- src/main/java/com/rainbowbridge/reborn/domain/Payment.java
+++ src/main/java/com/rainbowbridge/reborn/domain/Payment.java
@@ -1,4 +1,19 @@
 package com.rainbowbridge.reborn.domain;
 
 public enum Payment {
+
+    KAKAO_PAY("카카오페이"),
+    NAVER_PAY("네이버페이"),
+    CARD("신용/체크카드");
+
+    private final String displayName;
+
+    Payment(String displayName) {
+        this.displayName = displayName;
+    }
+
+    public String getDisplayName() {
+        return displayName;
+    }
+
 }
