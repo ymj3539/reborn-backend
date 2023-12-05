a7d4c8f838a1085473f3bac7a7566cf73a00ea83
Gomson-E <gomsonixx@gmail.com>
2023-12-06 04:43:42 +0900

    FEAT : Card Enum 값 설정

--- src/main/java/com/rainbowbridge/reborn/domain/Card.java
+++ src/main/java/com/rainbowbridge/reborn/domain/Card.java
@@ -1,4 +1,22 @@
 package com.rainbowbridge.reborn.domain;
 
 public enum Card {
+
+    KOOKMIN_CARD("국민카드"),
+    SAMSUNG_CARD("삼성카드"),
+    LOTTE_CARD("롯데카드"),
+    WOORI_CARD("우리카드"),
+    SHINHAN_CARD("신한카드"),
+    HYUNDAI_CARD("현대카드");
+
+    private final String displayName;
+
+    Card(String displayName) {
+        this.displayName = displayName;
+    }
+
+    public String getDisplayName() {
+        return displayName;
+    }
+
 }
