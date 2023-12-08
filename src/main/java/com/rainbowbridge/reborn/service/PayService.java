66cb08dc922b1fd42f6b816e9c6ab5bdde52b4cb
Gomson-E <gomsonixx@gmail.com>
2023-12-09 03:01:53 +0900

    FEAT : 결제 하기 기능

--- src/main/java/com/rainbowbridge/reborn/service/PayService.java
+++ src/main/java/com/rainbowbridge/reborn/service/PayService.java
@@ -1,19 +1,51 @@
 package com.rainbowbridge.reborn.service;
 
+import com.rainbowbridge.reborn.domain.Company;
 import com.rainbowbridge.reborn.domain.Pay;
+import com.rainbowbridge.reborn.domain.Pet;
+import com.rainbowbridge.reborn.domain.Product;
+import com.rainbowbridge.reborn.domain.Reservation;
+import com.rainbowbridge.reborn.domain.User;
 import com.rainbowbridge.reborn.dto.pay.PayAddRequestDto;
+import com.rainbowbridge.reborn.dto.pay.PayAddResponseDto;
+import com.rainbowbridge.reborn.dto.pet.PetAddRequestDto;
+import com.rainbowbridge.reborn.dto.reservation.ReservationAddRequestDto;
 import com.rainbowbridge.reborn.repository.PayRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
+
+import javax.servlet.http.HttpSession;
 
 @Service
 @RequiredArgsConstructor
 public class PayService {
 
     private final PayRepository payRepository;
+    private final PetService petService;
+    private final ProductService productService;
+    private final ReservationService reservationService;
+    private final CommonService commonService;
 
     public Pay addPay(PayAddRequestDto payDto) {
         return payRepository.save(payDto.toEntity());
+    }
+
+    public PayAddResponseDto addPayAndReservationAndPet(PayAddRequestDto payDto, ReservationAddRequestDto reservationDto, PetAddRequestDto petDto, HttpSession session) {
+        User user = (User) session.getAttribute("user");
+        Pet pet = petService.addPet(petDto, user);
+        Pay pay = addPay(payDto);
+        Product product = productService.getProduct(reservationDto.getProduct_id());
+        Company company = product.getCompany();
+        Reservation reservation = reservationService.addReservation(reservationDto, pet, pay, user, product, company);
+
+        return PayAddResponseDto.builder()
+                .payDt(pay.getPayDt())
+                .reservationDate(commonService.convertDateFormat(reservation.getDate()))
+                .reservationTime(commonService.convertHourTo12HourFormat(reservation.getTime()))
+                .companyName(company.getName())
+                .productName(product.getName())
+                .totalPrice(pay.getTotalPrice())
+                .build();
     }
 
 }
