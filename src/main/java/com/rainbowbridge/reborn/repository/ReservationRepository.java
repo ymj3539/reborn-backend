c7107e012d8ac6bf5ee73951685650e9b660b509
Gomson-E <gomsonixx@gmail.com>
2023-12-09 20:20:09 +0900

    FEAT : 다가오는 예약 조회 기능

--- src/main/java/com/rainbowbridge/reborn/repository/ReservationRepository.java
+++ src/main/java/com/rainbowbridge/reborn/repository/ReservationRepository.java
@@ -1,8 +1,12 @@
 package com.rainbowbridge.reborn.repository;
 
 import com.rainbowbridge.reborn.domain.Reservation;
+import com.rainbowbridge.reborn.domain.User;
 import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.List;
 
 public interface ReservationRepository extends JpaRepository<Reservation, Long> {
 
+    List<Reservation> findAllByUser(User user);
 }
--- src/main/java/com/rainbowbridge/reborn/service/ReservationService.java
+++ src/main/java/com/rainbowbridge/reborn/service/ReservationService.java
@@ -7,15 +7,48 @@ import com.rainbowbridge.reborn.domain.Product;
 import com.rainbowbridge.reborn.domain.Reservation;
 import com.rainbowbridge.reborn.domain.User;
 import com.rainbowbridge.reborn.dto.reservation.ReservationAddRequestDto;
+import com.rainbowbridge.reborn.dto.reservation.UpcomingReservationResponseDto;
 import com.rainbowbridge.reborn.repository.ReservationRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
+
+import javax.servlet.http.HttpSession;
+import java.time.LocalDateTime;
+import java.time.LocalTime;
+import java.util.List;
 
 @Service
 @RequiredArgsConstructor
 public class ReservationService {
 
     private final ReservationRepository reservationRepository;
+    private final CommonService commonService;
+
+    public UpcomingReservationResponseDto getUpcomingReservation(HttpSession session) {
+        User user = (User) session.getAttribute("user");
+
+        List<Reservation> reservations = reservationRepository.findAllByUser(user);
+
+        if (reservations.isEmpty()) {
+            return null;
+        }
+
+        Reservation upcomingReservation = reservations.get(reservations.size() - 1);
+
+        LocalDateTime now = LocalDateTime.now();
+        LocalDateTime reservationDateTime = LocalDateTime.of(upcomingReservation.getDate(), LocalTime.of(upcomingReservation.getTime(), 0));
+
+        if (reservationDateTime.isBefore(now)) {
+            return null;
+        }
+
+        return UpcomingReservationResponseDto.builder()
+                .companyName(upcomingReservation.getCompany().getName())
+                .reservationDate(commonService.convertLocalDateFormat(upcomingReservation.getDate()))
+                .reservationTime(commonService.convertTimeFormat(upcomingReservation.getTime()))
+                .build();
+
+    }
 
     public Reservation addReservation(ReservationAddRequestDto reservationDto, Pet pet, Pay pay, User user, Product product, Company company) {
         return reservationRepository.save(reservationDto.toEntity(pet, pay, user, product, company));
