49868fa37bc8093aae4aa8ccebe197b0ce7fde26
Gomson-E <gomsonixx@gmail.com>
2023-12-10 16:58:59 +0900

    UPDATE : 회원가입, 로그인 기능 - LoginResponseDto 반환 기능 추가

--- src/main/java/com/rainbowbridge/reborn/service/UserService.java
+++ src/main/java/com/rainbowbridge/reborn/service/UserService.java
@@ -2,6 +2,7 @@ package com.rainbowbridge.reborn.service;
 
 import com.rainbowbridge.reborn.domain.User;
 import com.rainbowbridge.reborn.dto.user.LoginDto;
+import com.rainbowbridge.reborn.dto.user.LoginResponseDto;
 import com.rainbowbridge.reborn.dto.user.UserAddDto;
 import com.rainbowbridge.reborn.repository.UserRepository;
 import lombok.RequiredArgsConstructor;
@@ -24,14 +25,15 @@ public class UserService {
     }
 
     @Transactional
-    public void addUser(UserAddDto dto, HttpSession session){
+    public LoginResponseDto addUser(UserAddDto dto, HttpSession session){
         String encodedPassword = UserSha256.encrypt(dto.getPassword());
         User user = dto.toEntity(encodedPassword);
 
         session.setAttribute("user", userRepository.save(user));
+        return toLoginResponseDto(user);
     }
 
-    public void loginUser(LoginDto dto, HttpSession session) {
+    public LoginResponseDto loginUser(LoginDto dto, HttpSession session) {
         User user = userRepository.findById(dto.getId())
                 .orElseThrow(() -> new EntityNotFoundException("아이디가 존재하지 않습니다."));
 
@@ -41,5 +43,18 @@ public class UserService {
         }
 
         session.setAttribute("user", user);
+
+        return toLoginResponseDto(user);
+    }
+
+    public LoginResponseDto toLoginResponseDto(User user) {
+        return LoginResponseDto.builder()
+                .id(user.getId())
+                .name(user.getName())
+                .phoneNum(user.getPhoneNum())
+                .birthday(user.getBirthday())
+                .gender(user.getGender())
+                .address(user.getAddress())
+                .build();
     }
 }
