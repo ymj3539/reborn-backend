7fcbe74117bf3f3dde319250ad9d98eac128218e
Gomson-E <gomsonixx@gmail.com>
2023-12-10 23:30:37 +0900

    FEAT : 현재 예약 중 여부 조회 기능

--- src/main/java/com/rainbowbridge/reborn/repository/ReservationRepository.java
+++ src/main/java/com/rainbowbridge/reborn/repository/ReservationRepository.java
@@ -1,5 +1,6 @@
 package com.rainbowbridge.reborn.repository;
 
+import com.rainbowbridge.reborn.domain.Company;
 import com.rainbowbridge.reborn.domain.Reservation;
 import com.rainbowbridge.reborn.domain.User;
 import org.springframework.data.jpa.repository.JpaRepository;
@@ -9,4 +10,5 @@ import java.util.List;
 public interface ReservationRepository extends JpaRepository<Reservation, Long> {
 
     List<Reservation> findAllByUser(User user);
+    List<Reservation> findAllByUserAndCompany(User user, Company company);
 }
--- src/main/java/com/rainbowbridge/reborn/service/ReservationService.java
+++ src/main/java/com/rainbowbridge/reborn/service/ReservationService.java
@@ -7,6 +7,7 @@ import com.rainbowbridge.reborn.domain.Pet;
 import com.rainbowbridge.reborn.domain.Product;
 import com.rainbowbridge.reborn.domain.Reservation;
 import com.rainbowbridge.reborn.domain.User;
+import com.rainbowbridge.reborn.dto.reservation.CheckReservationResponseDto;
 import com.rainbowbridge.reborn.dto.reservation.ReservationAddRequestDto;
 import com.rainbowbridge.reborn.dto.reservation.UpcomingReservationResponseDto;
 import com.rainbowbridge.reborn.repository.ReservationRepository;
@@ -23,6 +24,31 @@ import java.util.List;
 public class ReservationService {
 
     private final ReservationRepository reservationRepository;
+
+    public CheckReservationResponseDto checkReservation(Company company, HttpSession session) {
+        User user = (User) session.getAttribute("user");
+
+        if (user == null) {
+            return null;
+        }
+
+        List<Reservation> reservations = reservationRepository.findAllByUserAndCompany(user, company);
+
+        if (reservations.isEmpty()) {
+            return toCheckReservationResponseDto(false, null);
+        }
+
+        Reservation upcomingReservation = reservations.get(reservations.size() - 1);
+
+        LocalDateTime now = LocalDateTime.now();
+        LocalDateTime reservationDateTime = LocalDateTime.of(upcomingReservation.getDate(), LocalTime.of(upcomingReservation.getTime(), 0));
+
+        if (reservationDateTime.isBefore(now)) {
+            return toCheckReservationResponseDto(false, null);
+        }
+
+        return toCheckReservationResponseDto(true, Utils.convertLocalDateFormat(upcomingReservation.getDate()));
+    }
 
     public UpcomingReservationResponseDto getUpcomingReservation(HttpSession session) {
         User user = (User) session.getAttribute("user");
@@ -59,6 +85,13 @@ public class ReservationService {
 
     public Reservation addReservation(ReservationAddRequestDto reservationDto, Pet pet, Pay pay, User user, Product product, Company company) {
         return reservationRepository.save(reservationDto.toEntity(pet, pay, user, product, company));
+    }
+
+    private CheckReservationResponseDto toCheckReservationResponseDto(boolean reservationYN, String reservationDate) {
+        return CheckReservationResponseDto.builder()
+                .reservationYN(reservationYN)
+                .reservationDate(reservationDate)
+                .build();
     }
 
 }
