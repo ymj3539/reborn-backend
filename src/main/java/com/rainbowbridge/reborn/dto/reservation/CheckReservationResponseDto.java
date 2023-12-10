2bd7f6a343921a96283157ceb5136bb6aa98e978
Gomson-E <gomsonixx@gmail.com>
2023-12-10 23:21:59 +0900

    FEAT : 현재 예약 중 여부 조회 응답 DTO

---
+++ src/main/java/com/rainbowbridge/reborn/dto/reservation/CheckReservationResponseDto.java
@@ -0,0 +1,18 @@
+package com.rainbowbridge.reborn.dto.reservation;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+public class CheckReservationResponseDto {
+
+    private boolean reservationYN;
+
+    private String reservationDate;
+
+}
