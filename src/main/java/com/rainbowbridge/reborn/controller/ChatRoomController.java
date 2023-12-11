639f255f4e56be7edb9fb2933548a0ccdcc35329
Gomson-E <gomsonixx@gmail.com>
2023-12-11 17:50:10 +0900

    FEAT : 채팅방 목록 조회 응답 Controller

--- src/main/java/com/rainbowbridge/reborn/controller/ChatRoomController.java
+++ src/main/java/com/rainbowbridge/reborn/controller/ChatRoomController.java
@@ -1,15 +1,18 @@
 package com.rainbowbridge.reborn.controller;
 
 import com.rainbowbridge.reborn.dto.chatContent.ChatContentResponseDto;
+import com.rainbowbridge.reborn.dto.chatRoom.ChatRoomListDto;
 import com.rainbowbridge.reborn.service.ChatRoomService;
 import io.swagger.annotations.ApiOperation;
 import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import javax.servlet.http.HttpSession;
+import java.util.List;
 
 @RestController
 @RequiredArgsConstructor
@@ -17,6 +20,13 @@ import javax.servlet.http.HttpSession;
 public class ChatRoomController {
 
     private final ChatRoomService chatRoomService;
+
+    @GetMapping
+    @ApiOperation(value = "채팅방 목록 조회")
+    public List<ChatRoomListDto> getList(HttpSession session) {
+        return chatRoomService.getChatRoomList(session);
+    }
+
 
     @PostMapping("/{companyId}")
     @ApiOperation(value = "상담 하기")
