bcabd872ca50b19f45f48b9513535f7be52d927a
Gomson-E <gomsonixx@gmail.com>
2024-01-02 00:28:48 +0900

    FEAT : 토큰 DTO

---
+++ src/main/java/com/rainbowbridge/reborn/dto/token/JwtToken.java
@@ -0,0 +1,18 @@
+package com.rainbowbridge.reborn.dto.token;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+public class JwtToken {
+
+    private String grantType;
+    private String accessToken;
+    private String refreshToken;
+
+}
