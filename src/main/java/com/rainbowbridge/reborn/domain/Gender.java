d621f73b043816ee7985608845872ee3cc1ca875
Gomson-E <gomsonixx@gmail.com>
2024-01-09 15:27:25 +0900

    UPDATE : Gender enum - displayName 추가

--- src/main/java/com/rainbowbridge/reborn/domain/Gender.java
+++ src/main/java/com/rainbowbridge/reborn/domain/Gender.java
@@ -1,6 +1,17 @@
 package com.rainbowbridge.reborn.domain;
 
 public enum Gender {
-    MALE,
-    FEMALE
+    MALE("남성"),
+    FEMALE("여성");
+
+    private final String displayName;
+
+    Gender(String displayName) {
+        this.displayName = displayName;
+    }
+
+    public String getDisplayName() {
+        return displayName;
+    }
+
 }
