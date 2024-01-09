90bf81c98bd8af46ccd5197ee70ab3b0087dc32b
Gomson-E <gomsonixx@gmail.com>
2024-01-09 17:04:51 +0900

    FEAT : 사용자 정보 수정 요청 DTO

---
+++ src/main/java/com/rainbowbridge/reborn/dto/user/UserUpdateDto.java
@@ -0,0 +1,39 @@
+package com.rainbowbridge.reborn.dto.user;
+
+import com.rainbowbridge.reborn.domain.Gender;
+import io.swagger.annotations.ApiModelProperty;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.time.LocalDate;
+
+@Data
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+public class UserUpdateDto {
+
+    @ApiModelProperty(value = "사용자 이름")
+    private String name;
+
+    @ApiModelProperty(value = "사용자 전화번호")
+    private String phoneNum;
+
+    @ApiModelProperty(value = "사용자 생일")
+    private LocalDate birthday;
+
+    @ApiModelProperty(value = "사용자 성별")
+    private Gender gender;
+
+    @ApiModelProperty(value = "사용자 우편번호")
+    private String postalCode;
+
+    @ApiModelProperty(value = "사용자 기본주소")
+    private String baseAddress;
+
+    @ApiModelProperty(value = "사용자 상세주소")
+    private String detailAddress;
+
+}
