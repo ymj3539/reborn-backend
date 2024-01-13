d2736129871d7f3166c8536c755d75a63679eded
Gomson-E <gomsonixx@gmail.com>
2024-01-13 19:13:06 +0900

    FEAT : 장례 번들 추천 요청 DTO

---
+++ src/main/java/com/rainbowbridge/reborn/dto/bundle/BundleRecommendRequestDto.java
@@ -0,0 +1,42 @@
+package com.rainbowbridge.reborn.dto.bundle;
+
+import io.swagger.annotations.ApiModelProperty;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import javax.validation.constraints.NotNull;
+
+@Data
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+public class BundleRecommendRequestDto {
+
+    @ApiModelProperty(value = "가까운 순 YN", example = "true")
+    @NotNull
+    private boolean nearYN;
+
+    @ApiModelProperty(value = "구매 많은 YN", example = "true")
+    @NotNull
+    private boolean mostSoldYN;
+
+    @ApiModelProperty(value = "평이 좋은 YN", example = "true")
+    @NotNull
+    private boolean wellRatedYN;
+
+    @ApiModelProperty(value = "저렴한 YN", example = "true")
+    @NotNull
+    private boolean cheapYN;
+
+    @ApiModelProperty(value = "사용자 위치 위도", example = "35.2388660")
+    private double userLatitude;
+
+    @ApiModelProperty(value = "사용자 위치 경도", example = "129.222829")
+    private double userLongitude;
+
+    @ApiModelProperty(value = "액세스 토큰")
+    private String token;
+
+}
