946e5bc018f81cb463e5ab1d354bb44e4526ca89
Gomson-E <gomsonixx@gmail.com>
2024-01-13 15:36:17 +0900

    DELETE : Pay 관련 기능

--- src/main/java/com/rainbowbridge/reborn/service/ReservationService.java
+++ src/main/java/com/rainbowbridge/reborn/service/ReservationService.java
@@ -2,13 +2,9 @@ package com.rainbowbridge.reborn.service;
 
 import com.rainbowbridge.reborn.Utils;
 import com.rainbowbridge.reborn.domain.Company;
-import com.rainbowbridge.reborn.domain.Pay;
-import com.rainbowbridge.reborn.domain.Pet;
-import com.rainbowbridge.reborn.domain.Product;
 import com.rainbowbridge.reborn.domain.Reservation;
 import com.rainbowbridge.reborn.domain.User;
 import com.rainbowbridge.reborn.dto.reservation.CheckReservationResponseDto;
-import com.rainbowbridge.reborn.dto.reservation.ReservationAddRequestDto;
 import com.rainbowbridge.reborn.dto.reservation.UpcomingReservationResponseDto;
 import com.rainbowbridge.reborn.repository.ReservationRepository;
 import lombok.RequiredArgsConstructor;
@@ -80,10 +76,6 @@ public class ReservationService {
                 .reservationTime(Utils.convertTimeFormat(upcomingReservation.getTime()))
                 .build();
 
-    }
-
-    public Reservation addReservation(ReservationAddRequestDto reservationDto, Pet pet, Pay pay, User user, Product product, Company company) {
-        return reservationRepository.save(reservationDto.toEntity(pet, pay, user, product, company));
     }
 
     private boolean isReservationExpired(Reservation reservation) {
