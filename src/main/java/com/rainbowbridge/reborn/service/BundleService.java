0f6cc87c8d7da5241534ce03031467c3234b5eeb
Gomson-E <gomsonixx@gmail.com>
2024-01-16 17:29:29 +0900

    FEAT : 번들 상세 조회 기능

--- src/main/java/com/rainbowbridge/reborn/service/BundleService.java
+++ src/main/java/com/rainbowbridge/reborn/service/BundleService.java
@@ -4,11 +4,15 @@ import com.rainbowbridge.reborn.Utils;
 import com.rainbowbridge.reborn.domain.Bundle;
 import com.rainbowbridge.reborn.dto.bundle.BundleRecommendListDto;
 import com.rainbowbridge.reborn.dto.bundle.BundleRecommendRequestDto;
+import com.rainbowbridge.reborn.dto.bundle.BundleResponseDto;
+import com.rainbowbridge.reborn.dto.product.ProductListDto;
+import com.rainbowbridge.reborn.dto.supplement.SupplementListDto;
 import com.rainbowbridge.reborn.repository.BundleRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
+import javax.persistence.EntityNotFoundException;
 import java.util.Comparator;
 import java.util.List;
 import java.util.stream.Collectors;
@@ -20,6 +24,37 @@ public class BundleService {
 
     private final BundleRepository bundleRepository;
     private final HeatService heatService;
+
+    public BundleResponseDto getBundle(Long bundleId) {
+        Bundle bundle = bundleRepository.findById(bundleId)
+                .orElseThrow(() -> new EntityNotFoundException("존재하지 않는 번들입니다."));
+
+        List<ProductListDto> products = bundle.getProducts().stream()
+                .map(product -> ProductListDto.builder()
+                        .imagePath(Utils.getImagePath(product.getName()))
+                        .name(product.getName())
+                        .build())
+                .collect(Collectors.toList());
+
+        List<SupplementListDto> supplements = bundle.getCompany().getSupplements().stream()
+                .map(supplement -> SupplementListDto.builder()
+                        .id(supplement.getId())
+                        .name(supplement.getName())
+                        .price(supplement.getPrice())
+                        .intro(supplement.getIntro())
+                        .imagePath(Utils.getImagePath(supplement.getName()))
+                        .build())
+                .collect(Collectors.toList());
+
+        return BundleResponseDto.builder()
+                .imagePath(Utils.getImagePath(bundle.getName()))
+                .companyName(bundle.getCompany().getName())
+                .bundleName(bundle.getName())
+                .price(bundle.getPrice())
+                .products(products)
+                .companySupplements(supplements)
+                .build();
+    }
 
     public List<BundleRecommendListDto> getRecommendationList(BundleRecommendRequestDto dto, int limit) {
         List<Bundle> bundles = bundleRepository.findAll();
